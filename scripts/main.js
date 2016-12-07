e137edb04b568f43ca484695d91c85f808a11db1
Amy Mather <minigirlgeek@gmail,com>
2016-12-07 18:29:46 +0000

    Put JS script into a separate file.

---
+++ scripts/main.js
@@ -0,0 +1,66 @@
+var colour = "rgb(255, 255, 255)"
+var row;
+var rows;
+var state = 0;
+var wide = 15;
+var coords;
+
+function updateRows(state, element) {
+    coords = [element.x.animVal.value / wide, element.y.animVal.value / wide];
+    rows[coords[1]][coords[0]] = state;
+}
+
+function colourChange(element) {
+    if (element.style.fill == "rgb(255, 255, 255)"){
+        colour = "rgb(0, 0, 0)";
+        state = 1;
+        element.style.fill = colour;
+        updateRows(state, element);
+    }
+    else {
+        colour = "rgb(255, 255, 255)";
+        state = 0;
+        element.style.fill = colour;
+        updateRows(state, element);
+    }
+}
+
+function continuedColourChange(element, colour) {
+    element.style.fill = colour;
+    updateRows(state, element);
+}
+
+function drawGrid(width, height) {
+    var element = document.getElementById("svg");
+    element.parentNode.removeChild(element);
+    var svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+    var svgNS = svg.namespaceURI;
+
+    rows = [];
+    for (y = 0; y < height; y++) {
+        row = [];
+        for (x = 0; x < width; x++) {
+            row.push(0);
+            var rect = document.createElementNS(svgNS, 'rect')
+            rect.setAttribute('onmousedown', 'colourChange(this)');
+            rect.setAttribute('x', wide * x);
+            rect.setAttribute('y', wide * y);
+            rect.setAttribute('style','fill:#ffffff; cursor:pointer;');
+            rect.setAttribute('width', wide);
+            rect.setAttribute('height', wide);
+            rect.setAttribute('stroke','#000000');
+            rect.addEventListener("mouseover", function(e){
+                if(e.buttons == 1 || e.buttons == 3){
+                    continuedColourChange(this, colour);
+                    }
+                })
+            svg.appendChild(rect);
+            document.body.appendChild(svg);
+        }
+        rows.push(row);
+    }
+    console.log(rows);
+    svg.setAttribute('width',  wide * width);
+    svg.setAttribute('height', wide * height);
+    svg.setAttribute('id', 'svg');
+}

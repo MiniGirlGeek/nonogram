265271ad43e44bc45467378903576b95a869e25c
Amy Mather <minigirlgeek@gmail,com>
2016-12-09 17:11:27 +0000

    Pass the data from javascript to python and process it to work out the puzzle template numbers.

--- main.py
+++ main.py
@@ -1,9 +1,63 @@
 from flask import Flask, request, render_template
 app = Flask(__name__)
 
+def switch(grid):
+	new_grid = []
+	for y in range(len(grid)):
+		row = []
+		for x in range(len(grid[y])):
+			row.append(0)
+		new_grid.append(row)
+
+	for y in range(len(grid)):
+		for x in range(len(grid[y])):
+			new_grid[x][y] = grid[y][x]
+	return new_grid
+
+def find_groups(grid):
+	values = []
+	for y in grid:
+		count = 0
+		rowdata = []
+		for x in y:
+			if x == '1':
+				count += 1
+			else:
+				if count != 0:
+					rowdata.append(count)
+				count = 0
+		if count != 0:
+			rowdata.append(count)
+		values.append(rowdata)
+	return values
+
+def draw_puzzle(inp):
+	i = 0
+	grid = []
+	colvalues = []
+	rowvalues = []
+
+	#converting the data from the javascript backi into a 2D list
+	data = str(inp).split(',')
+	for y in range(int(request.form['height'])):
+		row = []
+		for x in range(int(request.form['width'])):
+			row.append(data[i])
+			i += 1
+		grid.append(row)
+
+	#work out what the puzzle template numbers will be
+	rowvalues = find_groups(grid)
+	colvalues = find_groups(switch(grid))
+
+	#create a svg element to return
+	
+	return str((rowvalues, colvalues))
+
+
 @app.route('/', methods=['GET', 'POST'])
 def hello_world():
 	if request.method == 'POST':
-		return 'orange'
+		return draw_puzzle(request.form['data'])
 	else:
 		return render_template('index.html')
---
+++ static/scripts/main.js
@@ -0,0 +1,89 @@
+  /*                    /
+ / Variable definition /
+/                    */
+var colour = "rgb(255, 255, 255)"
+var row;
+var rows;
+var state = 0;
+var wide = 15;
+var coords;
+
+/*Function definition*/
+function updateRows(state, element) {
+	/*gets the coordinates of the cell that has just been clicked*/
+    coords = [element.x.animVal.value / wide, element.y.animVal.value / wide];
+	/*updates the row grid to reflect the new state of that cell*/
+    rows[coords[1]][coords[0]] = state;
+}
+
+function post() {
+    var form = document.getElementById("nonogram");
+    var hiddenField = document.createElement("input");
+    hiddenField.setAttribute("type", "hidden");
+    hiddenField.setAttribute("name", "data");
+    hiddenField.setAttribute("value", rows);
+    form.appendChild(hiddenField);
+    form.submit();
+}
+
+function colourChange(element) {
+	/*checks to see what the current colour of the cell is*/
+    if (element.style.fill == "rgb(255, 255, 255)"){
+		/*flips it's colour*/
+		/*updates the state and colour variablke accordingly*/
+        colour = "rgb(0, 0, 0)";
+        state = 1;
+		/*updates the cell's fill*/
+
+        element.style.fill = colour;
+        updateRows(state, element);
+    }
+    else {
+        colour = "rgb(255, 255, 255)";
+        state = 0;
+        element.style.fill = colour;
+        updateRows(state, element);
+    }
+}
+
+function continuedColourChange(element, colour) {
+    element.style.fill = colour;
+    updateRows(state, element);
+}
+
+function drawGrid(width, height, wide) {
+	if (confirm("Warning, if you have already started drawing this will clear your progress!") == true) {
+		var element = document.getElementById("svg");
+		element.parentNode.removeChild(element);
+		var svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+		var svgNS = svg.namespaceURI;
+
+		rows = [];
+		for (y = 0; y < height; y++) {
+			row = [];
+			for (x = 0; x < width; x++) {
+				row.push(0);
+				var rect = document.createElementNS(svgNS, 'rect')
+				rect.setAttribute('onmousedown', 'colourChange(this)');
+				rect.setAttribute('x', wide * x);
+				rect.setAttribute('y', wide * y);
+				rect.setAttribute('style','fill:#ffffff; cursor:pointer;');
+				rect.setAttribute('width', wide);
+				rect.setAttribute('height', wide);
+				rect.setAttribute('stroke','#000000');
+				rect.addEventListener("mouseover", function(e){
+					if(e.buttons == 1 || e.buttons == 3){
+						continuedColourChange(this, colour);
+						}
+					})
+				svg.appendChild(rect);
+				document.getElementById('grid').appendChild(svg);
+			}
+			rows.push(row);
+		}
+		console.log(rows);
+		svg.setAttribute('width',  wide * width);
+		svg.setAttribute('height', wide * height);
+		svg.setAttribute('id', 'svg');
+	}
+}

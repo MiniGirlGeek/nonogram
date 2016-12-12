5b114873412b35c37c748cede64ccd19b71ecfa5
Amy Mather <minigirlgeek@gmail,com>
2016-12-12 23:19:21 +0000

    Draw puzzle template data for cols.

--- main.py
+++ main.py
@@ -2,6 +2,84 @@ from flask import Flask, request, render_template
 #import svgwrite
 app = Flask(__name__)
 
+svgtemplate = '''
+<svg id="puzzle_template" width="{0}" height="{1}">
+	{2}
+</svg>
+ '''
+
+recttemplate = '''
+	<rect x="{0}"  y="{1}" width="{2}" height={2} stroke="#000000" fill="white"></rect>
+	{3}
+'''
+
+texttemplate = '''
+	<text x="{0}" y="{1}" font-family="Verdana" font-size="10" fill="black" text-anchor="middle">{2}</text>
+	{3}
+'''
+
+def draw_grid(data):
+	rowval = data[0][:]
+	rowdat = data[0][:]
+	rowval.sort(key=lambda x: len(x))
+	colval = data[1][:]
+	coldat = data[1][:]
+	print(coldat)
+	colval.sort(key=lambda x: len(x))
+	rowvallen = len(rowval[-1])
+	colvallen = len(colval[-1])
+	print(coldat)
+	width = (int(request.form['width']) + rowvallen) * int(request.form['length'])
+	height = (int(request.form['height']) + colvallen) * int(request.form['length'])
+	grid = svgtemplate.format(width, height, '{0}')
+	i = 0
+
+	startx = 0
+	starty = 0
+	xpos = startx
+	ypos = starty
+
+	#display column values
+	startx = rowvallen * int(request.form['length'])
+	xpos = startx
+	starty = colvallen * int(request.form['length'])
+	ypos = starty
+	for y in range(colvallen):
+		for x in range(int(request.form['width'])):
+			try:
+				text = texttemplate.format(xpos + (int(request.form['length']) / 2),
+										   ypos + 12,
+										   coldat[x][y], '{0}')
+				rect = recttemplate.format(xpos, ypos, int(request.form['length']), text)
+				grid = grid.format(rect, '{0}')
+			except:
+				pass
+			xpos += int(request.form['length'])
+		xpos = startx
+		ypos -= int(request.form['length'])
+
+	#display row values
+	startx = int(request.form['length'])
+	xpos = startx
+	starty = (colvallen + 1) * int(request.form['length'])
+	ypos = starty
+	for y in range(rowvallen):
+		for x in range(int(request.form['width'])):
+			try:
+				text = texttemplate.format(xpos + (int(request.form['length']) / 2),
+										   ypos + 12,
+										   rowdat[x][y], '{0}')
+				rect = recttemplate.format(xpos, ypos, int(request.form['length']), text)
+				grid = grid.format(rect, '{0}')
+			except:
+				pass
+			xpos -= int(request.form['length'])
+		xpos = startx
+		ypos += int(request.form['length'])
+
+	return grid
+
+
 def switch(grid):
 	new_grid = []
 	width = len(grid)
@@ -32,6 +110,10 @@ def find_groups(grid):
 		if count != 0:
 			rowdata.append(count)
 		values.append(rowdata)
+
+		for i in range(len(values)):
+			if values[i] == []:
+				values[i] = [0]
 	return values
 
 def draw_puzzle(inp):
@@ -54,7 +136,7 @@ def draw_puzzle(inp):
 	colvalues = find_groups(switch(grid))
 
 	#create a svg element to return
-
+	return draw_grid((rowvalues, colvalues))
 	return str((rowvalues, colvalues))
 
 

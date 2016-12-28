ebea52342fbb6383697a249fe602cca50fa8eb72
Amy Mather <minigirlgeek@gmail,com>
2016-12-28 20:53:07 +0000

    Add more pages.

--- main.py
+++ main.py
@@ -2,11 +2,13 @@ from flask import Flask, request, render_template
 import nonogram
 app = Flask(__name__)
 
-@app.route('/', methods=['GET', 'POST'])
-def hello_world():
+@app.route('/')
+def index():
+		return render_template('index.html')
+
+@app.route('/create', methods=['GET', 'POST'])
+def create():
 	if request.method == 'POST':
 		return nonogram.populate_grid(request)
-		#draw.test()
-		#draw.send_data(request)
 	else:
-		return render_template('index.html')
+		return render_template('create.html')

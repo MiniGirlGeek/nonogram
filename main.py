03d05bc683b72d54e768958d75e68315c3f3d6d7
Amy Mather <minigirlgeek@gmail,com>
2016-12-29 11:12:50 +0000

    Correct multiple functions with same name mistake.

--- main.py
+++ main.py
@@ -7,20 +7,20 @@ def index():
 		return render_template('index.html')
 
 @app.route('/solve')
-def index():
+def solve():
 		return render_template('index.html')
 
-		@app.route('/create', methods=['GET', 'POST'])
-		def create():
-			if request.method == 'POST':
-				return render_template('puzzle.html', puzzle=nonogram.populate_grid(request))
-			else:
-				return render_template('create.html')
+@app.route('/create', methods=['GET', 'POST'])
+def create():
+	if request.method == 'POST':
+		return render_template('puzzle.html', puzzle=nonogram.populate_grid(request))
+	else:
+		return render_template('create.html')
 
 @app.route('/login')
-def index():
+def login():
 		return render_template('index.html')
 
 @app.route('/signup')
-def index():
+def signup():
 		return render_template('index.html')
